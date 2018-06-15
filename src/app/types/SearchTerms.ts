f9c4efeb33fc2461756818b130ff5ddc32a14c93
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-06-15 13:07:50 +0200

    Another CheckRatingService rename

--- src/app/types/SearchTerms.ts
+++ src/app/types/SearchTerms.ts
@@ -1,10 +1,10 @@
-import {CheckRatingService} from '../services/check-rating.service';
+import {BoardRatingService} from '../services/board-rating.service';
 
 export class SearchTerms {
   rating: string;
   constructor(private _tags: string, private _pageSize: number, private _isSafe: boolean, private _isQuestionable: boolean, private _isExplicit: boolean ) {
-    const ratingService = new CheckRatingService();
-    this.rating = ratingService.checkRating(this._isSafe, this._isQuestionable, this._isExplicit);
+    const ratingService = new BoardRatingService();
+    this.rating = ratingService.checkKonachanRating(this._isSafe, this._isQuestionable, this._isExplicit);
   }
   get tags() {
     return this._tags;
