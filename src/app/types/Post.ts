9e8caeec6edd48c5d46c33496c70a8bdd7e17bff
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-07-01 23:43:09 +0200

    Created a common post type

---
+++ src/app/types/Post.ts
@@ -0,0 +1,12 @@
+export default class Posts {
+  id: number;
+  author: string;
+  created_at: number;
+  source: string;
+  tags: string;
+  preview_url: string;
+  preview_height: number;
+  preview_width: number;
+  file_url: string;
+  jpeg_url: string;
+}
